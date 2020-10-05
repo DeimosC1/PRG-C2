827dcf46b79d974003f8d3e09a8a755fca526f8e
DeimosC1 <62855417+DeimosC1@users.noreply.github.com>
2020-10-05 15:15:31 +0200

    Create java.java
    pismena

---
+++ java.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class java {
+    public static void main(String[] args) {
+        String pismena = "Bruh";
+
+
+            System.out.println(pismena.length());
+
+    }
+}
